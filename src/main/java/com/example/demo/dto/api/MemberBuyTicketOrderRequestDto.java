314cd235ebddbaacd3a5da44ba08e4aaf7e4c476
Ricter1229 <a095280403988@gmail.com>
2024-11-29 08:43:02 +0800

    add dto

---
+++ src/main/java/com/example/demo/dto/api/MemberBuyTicketOrderRequestDto.java
@@ -0,0 +1,22 @@
+package com.example.demo.dto.api;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Data
+public class MemberBuyTicketOrderRequestDto {
+	private Integer orderId;
+	private Integer memberId;
+	private Integer movieId;
+	private BigDecimal totalAmount;
+	private String status;
+	private MemberBuyTicketDetailRequestDto orderDetail;
+}
